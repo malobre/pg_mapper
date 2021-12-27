52e3915a4d04718564e84d5c38355ab916a19728
Maël Obréjan <mael.obrejan@protonmail.com>
2021-12-27 15:10:50 +0100

    Move everything to a single crate, add LICENSE

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,19 @@
 //!     }
 //! }
 //! ```
-pub use pg_mapper_derive::TryFromRow;
+use proc_macro::{self, TokenStream};
+use syn::parse_macro_input;
+
+mod expand;
+
+#[proc_macro_derive(TryFromRow)]
+pub fn derive_try_from_row(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input);
+
+    expand::try_from_row(input)
+        .unwrap_or_else(|error| error.to_compile_error())
+        .into()
+}
 
 #[cfg(test)]
 mod tests {
